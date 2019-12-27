63de62224a45bffc2e949f30f9cdb116cba39112
John Tran <johnnydattran@gmail.com>
2019-12-26 22:48:52 -0700

    Final version of IoT

---
+++ WebApp/react-app/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Amplify from 'aws-amplify';
+import {HashRouter} from "react-router-dom"
+import * as serviceWorker from './serviceWorker';
+import config from './config'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import './index.css';
+import App from './modules/App';
+
+Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    },
+    Storage: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID
+    }
+  });
+
+
+
+ReactDOM.render(<HashRouter>
+                    <App />
+                </HashRouter>,
+                document.getElementById('root'));
+serviceWorker.unregister();
---
+++ WebApp/react-app/src/modules/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Navigation from './Navigation';
+import Routes from './Routes';
+import './Basic.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false,
+      user:null,
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+      this.setState({ isAuthenticated: authenticated });
+  }
+  updateUser = user =>{
+    this.setState({user:user});
+  }
+
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      user: this.state.user,
+      userHasAuthenticated: this.userHasAuthenticated,
+      updateUser:this.updateUser,
+    };
+    return (
+      <div data-test="app">
+        <Navigation childProps={childProps}/>
+        <Routes childProps={childProps}/>
+      </div>
+    );
+  }
+}
+
+export default App;
